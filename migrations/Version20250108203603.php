fce45ada999ed1af9fa4a6b77dd919770ede2bbf
Mathias Ghanem <mathiasghanem05@gmail.com>
2025-01-10 19:44:06 +0100

    migration

--- migrations/Version20250108203603.php
+++ migrations/Version20250108203603.php
@@ -23,13 +23,15 @@ final class Version20250108203603 extends AbstractMigration
         $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, category_id BIGINT NOT NULL, pages INT NOT NULL, publication_date DATETIME NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE book_read (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, book_id BIGINT NOT NULL, rating NUMERIC(5, 2) DEFAULT NULL, description LONGTEXT DEFAULT NULL, is_read TINYINT(1) NOT NULL, cover VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE book');
-        $this->addSql('DROP TABLE book_read');
+        $this->addSql(sql: 'DROP TABLE book_read');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE user');
     }
 }
